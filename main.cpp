6f090b7d7843571e9b0cbe50a2c32e12cfe1996d
A01799600 <117095682+A01799600@users.noreply.github.com>
2022-11-06 14:18:00 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+double calcularIMC(double p, double a){
+    return p/ (a * a);
+}
+
+int main(void){
+
+    double peso,altura;
+    cout << "Ingrese el peso (Kg): " << endl;
+    cin >> peso;
+    cout << "Ingrese la altura (m): " << endl;
+    cin >> altura;
+    
+}
